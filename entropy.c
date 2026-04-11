b11fa629f592de5fe8823522f6d47deec83ee103
divyadarshan07 <divyadarshanrv@gmail.com>
2026-04-11 02:53:36 -0400

    Update entropy.c

--- entropy.c
+++ entropy.c
@@ -5,13 +5,13 @@ double calculate_entropy(const char *filename) {
     FILE *file = fopen(filename, "rb");
     if (!file) return 0;
 
-    int freq[256] = {0};
-    int total = 0;
-    unsigned char buffer[1024];
+    long long freq[256] = {0};
+    long long total = 0;
+    unsigned char buffer[4096];
 
     size_t bytesRead;
     while ((bytesRead = fread(buffer, 1, sizeof(buffer), file)) > 0) {
-        for (int i = 0; i < bytesRead; i++) {
+        for (size_t i = 0; i < bytesRead; i++) {
             freq[buffer[i]]++;
             total++;
         }
@@ -23,8 +23,7 @@ double calculate_entropy(const char *filename) {
 
     for (int i = 0; i < 256; i++) {
         if (freq[i] == 0) continue;
-
-        double p = (double)freq[i] / total;
+        double p = (double)freq[i] / (double)total;
         entropy -= p * log2(p);
     }
 
